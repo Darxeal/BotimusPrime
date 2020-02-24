4e9363829aceb3b6974d07e109dfcaf9924aa05a
Jiří Pihrt <jira.pihrt@volny.cz>
2020-02-24 18:57:29 +0100

    Remove quick chats

--- agent.py
+++ agent.py
@@ -4,7 +4,6 @@ from rlutilities.simulation import Input
 from rlutilities.linear_algebra import norm
 
 from tools.drawing import DrawingTool
-from tools.quick_chats import QuickChatTool
 from rlbot.matchcomms.common_uses.set_attributes_message import handle_set_attributes_message
 from rlbot.matchcomms.common_uses.reply import reply_to
 
@@ -19,6 +18,7 @@ from utils.vector_math import distance
 from utils.game_info import GameInfo
 
 import time
+
 
 class BotimusPrime(BaseAgent):
     
@@ -46,14 +46,6 @@ class BotimusPrime(BaseAgent):
         self.draw: DrawingTool = DrawingTool(self.renderer)
 
         self.strategy = SoccarStrategy(self.info, self.draw)
-
-        # variables related to quick chats
-        self.chat = QuickChatTool(self)
-        self.last_ball_vel = 0
-        self.said_gg = False
-        self.last_time_said_all_yours = 0
-        self.num_of_our_goals_reacted_to = 0
-        self.num_of_their_goals_reacted_to = 0
 
     def get_output(self, packet: GameTickPacket):
         self.time = packet.game_info.seconds_elapsed
@@ -120,77 +112,6 @@ class BotimusPrime(BaseAgent):
         if self.RENDERING:
             self.draw.execute()
 
-        self.maybe_chat(packet)
-        self.chat.step(packet)
-
         return self.controls
-
-    def maybe_chat(self, packet: GameTickPacket):
-        chat = self.chat
-
-        for team in packet.teams:
-            if team.team_index == self.team:
-                our_score = team.score
-            else:
-                their_score = team.score
-
-        # last second goal
-        if their_score > self.num_of_their_goals_reacted_to or our_score > self.num_of_our_goals_reacted_to:
-            if abs(their_score - our_score) < 2 and packet.game_info.game_time_remaining < 5:
-                for _ in range(6):
-                    self.chat.send_random([
-                        chat.Reactions_OMG,
-                        chat.PostGame_Gg,
-                        chat.Reactions_HolyCow,
-                        chat.Reactions_NoWay,
-                        chat.Reactions_Wow,
-                        chat.Reactions_OMG
-                    ])
-
-        # they scored
-        if their_score > self.num_of_their_goals_reacted_to:
-            self.num_of_their_goals_reacted_to = their_score
-            for _ in range(2):
-                if self.last_ball_vel > 2000:
-                    self.chat.send_random([
-                        chat.Compliments_NiceShot,
-                        chat.Compliments_NiceOne,
-                        chat.Reactions_Wow,
-                        chat.Reactions_OMG,
-                        chat.Reactions_Noooo
-                    ])
-                else:
-                    self.chat.send_random([
-                        chat.Reactions_Whew,
-                        chat.Apologies_Whoops,
-                        chat.Apologies_Oops,
-                        chat.Apologies_Cursing
-                    ])
-
-        # we scored
-        if our_score > self.num_of_our_goals_reacted_to:
-            self.num_of_our_goals_reacted_to = our_score
-
-            if self.last_ball_vel > 3000:
-                self.chat.send(chat.Reactions_Siiiick)
-
-            if self.last_ball_vel < 300:
-                self.chat.send(chat.Compliments_WhatASave)
-
-        # game is over
-        if packet.game_info.is_match_ended and not self.said_gg:
-            self.said_gg = True
-
-            self.chat.send(chat.PostGame_Gg)
-            self.chat.send(chat.PostGame_WellPlayed)
-
-            if our_score < their_score:
-                self.chat.send(chat.PostGame_OneMoreGame)
-
-        # all yours :D
-        if self.time > self.last_time_said_all_yours + 40:
-            if isinstance(self.maneuver, ShadowDefense) and distance(self.info.my_car, self.info.ball) > 6000:
-                self.last_time_said_all_yours = self.time
-                self.chat.send(chat.Information_AllYours)
 
 
