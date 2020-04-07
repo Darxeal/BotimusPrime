a51c7cd4d9b1b680fab8b107272f48cb2737456d
Jiří Pihrt <jira.pihrt@volny.cz>
2020-04-07 19:23:01 +0200

    Improve AerialStrike

--- maneuvers/strikes/aerial_strike.py
+++ maneuvers/strikes/aerial_strike.py
@@ -1,17 +1,18 @@
 from typing import List
 
 from maneuvers.strikes.strike import Strike
-from rlutilities.linear_algebra import vec3, norm
+from rlutilities.linear_algebra import vec3, norm, normalize, look_at
 from rlutilities.mechanics import Aerial
 from rlutilities.simulation import Car, Ball
 from utils.drawing import DrawingTool
 from utils.game_info import GameInfo
 from utils.intercept import Intercept, estimate_time
-from utils.vector_math import ground_direction, angle_to, distance, ground_distance
+from utils.math import range_map
+from utils.vector_math import ground_direction, angle_to, distance, ground_distance, direction
 
 
 class AerialStrike(Strike):
-    MAX_DISTANCE_ERROR = 150
+    MAX_DISTANCE_ERROR = 50
 
     def __init__(self, car: Car, info: GameInfo, target: vec3 = None):
         self.aerial = Aerial(car)
@@ -20,14 +21,16 @@ class AerialStrike(Strike):
         self.arrive.allow_dodges_and_wavedashes = False
 
         self.aerialing = False
+        self.too_early = False
         self._flight_path: List[vec3] = []
 
     def intercept_predicate(self, car: Car, ball: Ball):
-        return ball.position[2] > 400 and estimate_time(car, ball.position, 1400) < ball.time - car.time
+        return ball.position[2] > 500 and ball.time - car.time > range_map(ball.position[2], 300, 1900, 0.8, 3.5)
 
     def configure(self, intercept: Intercept):
         super().configure(intercept)
-        self.aerial.target = intercept.position - ground_direction(intercept, self.target) * 50
+        self.aerial.target = intercept.position - direction(intercept, self.target) * 50
+        self.aerial.up = normalize(ground_direction(intercept, self.car) + vec3(0, 0, 0.5))
         self.aerial.arrival_time = intercept.time
 
     def simulate_flight(self, car: Car, write_to_flight_path=True) -> Car:
@@ -36,6 +39,7 @@ class AerialStrike(Strike):
         test_aerial.target = self.aerial.target
         test_aerial.arrival_time = self.aerial.arrival_time
         test_aerial.angle_threshold = self.aerial.angle_threshold
+        test_aerial.up = self.aerial.up
 
         if write_to_flight_path:
             self._flight_path.clear()
@@ -52,9 +56,11 @@ class AerialStrike(Strike):
 
     def step(self, dt):
         if self.aerialing:
+            self.aerial.target_orientation = look_at(direction(self.car, self.info.ball), self.car.up())
             self.aerial.step(dt)
             self.controls = self.aerial.controls
             self.finished = self.aerial.finished
+            print(self.controls.jump)
         else:
             super().step(dt)
 
@@ -63,11 +69,12 @@ class AerialStrike(Strike):
 
             # if the car ended up too far, we're too fast and we need to slow down
             if ground_distance(self.car, self.aerial.target) + 100 < ground_distance(self.car, simulated_car):
-                self.arrive.drive.target_speed = 0
+                # self.controls.throttle = -1
+                pass
 
             # if it ended up near the target, we could take off
             elif distance(simulated_car, self.aerial.target) < self.MAX_DISTANCE_ERROR:
-                if angle_to(self.car, self.aerial.target) < 0.1 or norm(self.car.velocity) < 500:
+                if angle_to(self.car, self.aerial.target) < 0.1 or norm(self.car.velocity) < 1000:
 
                     # extrapolate current state a small amount of time
                     future_car = Car(self.car)
@@ -82,11 +89,14 @@ class AerialStrike(Strike):
                     # this makes sure that we go for the most late possible aerials, which are the most effective
                     if distance(future_simulated_car, self.aerial.target) > self.MAX_DISTANCE_ERROR:
                         self.aerialing = True
+                    else:
+                        self.too_early = True
 
             else:
-                self.arrive.drive.target_speed = 2300
+                # self.controls.boost = True
+                self.controls.throttle = 1
 
     def render(self, draw: DrawingTool):
         super().render(draw)
-        draw.color(draw.lime if self.aerialing else draw.red)
+        draw.color(draw.lime if self.aerialing else (draw.orange if self.too_early else draw.red))
         draw.polyline(self._flight_path)
