0c5f715dfdaf55dc07630cd2a16b538112bef97b
Jiří Pihrt <jira.pihrt@volny.cz>
2020-06-03 19:53:35 +0200

    Tune jump magic numbers again

--- maneuvers/strikes/dodge_strike.py
+++ maneuvers/strikes/dodge_strike.py
@@ -29,7 +29,7 @@ class DodgeStrike(Strike):
             self.arrive.target = intercept.ground_pos - ground_direction(intercept, self.target) * 110
 
         additional_jump = clamp((intercept.ball.position[2]-92) / 500, 0, 1.5) * self.jump_time_multiplier
-        self.dodge.jump.duration = 0.05 + additional_jump
+        self.dodge.jump.duration = 0.07 + additional_jump
         self.dodge.target = intercept.ball.position
         self.arrive.additional_shift = additional_jump * 500
 
@@ -43,7 +43,7 @@ class DodgeStrike(Strike):
         else:
             super().step(dt)
             if (
-                self.arrive.arrival_time - self.car.time < self.dodge.jump.duration + 0.17
+                self.arrive.arrival_time - self.car.time < self.dodge.jump.duration + 0.13
                 and abs(self.arrive.drive.target_speed - norm(self.car.velocity)) < 1000
             ):
                 self.dodging = True
