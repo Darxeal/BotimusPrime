d82e0785fe87938820d30bfc0962e18ce58b2443
Jiří Pihrt <jira.pihrt@volny.cz>
2020-06-18 00:17:41 +0200

    Decrease max intercept height on CloseShot

--- maneuvers/strikes/close_shot.py
+++ maneuvers/strikes/close_shot.py
@@ -1,10 +1,14 @@
 from maneuvers.strikes.dodge_strike import DodgeStrike
+from rlutilities.simulation import Car, Ball
 from utils.intercept import Intercept
 from utils.math import abs_clamp
 
 
 class CloseShot(DodgeStrike):
     jump_time_multiplier = 1.1
+
+    def intercept_predicate(self, car: Car, ball: Ball):
+        return ball.position[2] < 250
 
     def configure(self, intercept: Intercept):
         self.target[0] = abs_clamp(self.intercept.ground_pos[0], 400)
